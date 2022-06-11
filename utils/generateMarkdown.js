39ac36455650c0560575bc81ab22f1f19560c6ae
Lindsey Sherrill <lindsey350@aol.com>
2022-06-10 20:59:53 -0400

    added markdown data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license === "None"){
+  if (license === "None") {
     return '';
   }
   return '* [License](#license)'
@@ -13,7 +13,7 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -26,23 +26,26 @@ function generateMarkdown(data) {
   ## Table of Contents
   
   * [Installation](#installation)
+  ${renderLicenseLink(data.projectLicense)}
 
   ## Installation 
-${renderLicenseLink(data.projectLicense)}
-  
+  ${data.installationInstructions}
+
   ## Usage
-  
+  ${data.usageInstructions}
   
   ## License
-
+  ${data.projectLicense}
 
   ## Contributing
-  
+  ${data.contributionInstructions}
   
   ## Tests
-  
+  ${data.testInstructions}
   
   ## Questions
+  ${data.gitHubUser}
+  ${data.eMail}
 
 `;
 }
